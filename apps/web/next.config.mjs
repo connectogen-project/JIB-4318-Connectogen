074d0ee41ef388a8881f33f817f0801e6ce2ac8c
Chadha, Arjan Singh <arjanchadha@gmail.com>
2024-11-17 01:34:16 -0500

    Fixed the relative paths by adjusting for the new compatability with TypeScript and Webpack

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+import {fileURLToPath} from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
     webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '@': path.resolve(__dirname),
-            '@/utils': path.resolve(__dirname, 'packages/ui/src/lib'),
-        };
+        config.resolve.alias['@'] = resolve(__dirname, 'packages/ui/src');
         return config;
     },
 };
