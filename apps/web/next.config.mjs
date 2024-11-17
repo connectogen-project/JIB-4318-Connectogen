a3b79cee0694da09e1c4204303f42cb612dfc8ce
Chadha, Arjan Singh <arjanchadha@gmail.com>
2024-11-17 01:20:46 -0500

    - Added subset to inter font in app/layout.tsx to fix npm build error - Added Paths and BaseUrl to tsconfig.json. Since it extended from the previous typescript version, there wasn’t a path alias config implemented. Adding path aliases will now make @/lib/utils work. -

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname),
+            '@/utils': path.resolve(__dirname, 'packages/ui/src/lib'),
+        };
+        return config;
+    },
+};
 
 export default nextConfig;
