db416e652a140017a9baa8e11fad1aa03d0d6efe
Guo, Patrick <patrickguo123@gmail.com>
2025-01-27 20:03:55 -0500

    CONNECT-85 Logout backend implemented but requires testing after login implementation

--- apps/web/app/lib/components/navbar.tsx
+++ apps/web/app/lib/components/navbar.tsx
@@ -4,9 +4,17 @@ import Link from "next/link";
 import Logo from "@repo/ui/components/logo";
 import { Button } from "./Button";
 import { InboxIcon, UserCircle } from "lucide-react";
+import { logoutUser } from "../../../../backend/controllers/users.controllers"
+import { useRouter } from 'next/router';
 
 export default function NavBar() {
     const pathname = usePathname();
+    const router = useRouter();
+
+    const onLogoutClick = async () => {
+        await logoutUser();
+        router.push('/login');
+    };
 
     return (
         <div className="flex items-center border-b border-border h-[68px] sticky top-0 z-10">
@@ -66,7 +74,10 @@ export default function NavBar() {
                         </Link>
                     </Button>
                     {/* need to add backend functionality to logout user */}
-                    <Button className="bg-foreground text-background px-4 py-2 rounded hover:bg-muted-foreground">
+                    <Button
+                        onClick={onLogoutClick}
+                        className="bg-foreground text-background px-4 py-2 rounded hover:bg-muted-foreground"
+                    >
                         <Link href="/login">
                             Logout
                         </Link>
