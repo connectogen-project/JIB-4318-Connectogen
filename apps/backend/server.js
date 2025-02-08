6d93813a15869433694fa3c495e28b4ed59e0c11
arjchad <arjanchadha@gmail.com>
2025-02-07 22:20:56 -0500

    CONNECT-135 forgot to git add

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -6,6 +6,7 @@ import { connectDB } from './config/db.js';
 
 import logRoutes from "./routes/logs.routes.js";
 import userRoutes from "./routes/users.routes.js";
+import mentorRoutes from './routes/mentor.routes.js';
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 
 app.use("/api/logs", logRoutes)
 app.use("/api/users", userRoutes)
+app.use('/api', mentorRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
