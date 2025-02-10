38b895bc172b8da47fc0ab483d2ed6ff9d81b567
Lloyd, Ella Megan Nalani <elloyd9@gatech.edu>
2025-02-09 22:29:12 -0500

    added shadcn navbar

--- apps/web/app/lib/components/navbar.tsx
+++ apps/web/app/lib/components/navbar.tsx
@@ -1,12 +1,24 @@
 "use client";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Logo from "@repo/ui/components/logo";
 import { Button } from "@repo/ui/components/ui/button";
 import { InboxIcon, UserCircle } from "lucide-react";
 
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    // NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    // NavigationMenuViewport,
+} from "@repo/ui/components/ui/navigation-menu"
+
+
 export default function NavBar() {
-    const pathname = usePathname();
+    // const pathname = usePathname();
 
     const handleLogout = async () => {
         try {
@@ -23,36 +35,41 @@ export default function NavBar() {
 
     return (
         <div className="flex w-full items-center border-b border-border h-[68px] sticky top-0 z-50 bg-white">
-            <div className="flex flex-grow">
+            <div className="flex flex-grow items-center">
                 <div className="mx-9"><Logo /></div>
-                <ul className="flex gap-x-4 grow">
-                    <li>
-                        <Link
-                            className={pathname.startsWith("/projects") ? "text-foreground" : "text-muted-foreground"}
-                            href="/projects"
-                        >
-                            Projects
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className={pathname === "/mentorship" ? "text-foreground" : "text-muted-foreground"}
-                            href="/mentorship"
-                        >
-                            Mentorship
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className={pathname === "/mentorship/logs" ? "text-foreground" : "text-muted-foreground"}
-                            href="/mentorship/logs"
-                        >
-                            Log
-                        </Link>
-                    </li>
-                </ul>
-
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
+                            <NavigationMenuContent className="flex flex-col p-4 w-[300px] md:w-[400px] lg:w-[500px]">
+                                <NavigationMenuLink
+                                    className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                    href="/projects"
+                                >
+                                    All Projects
+                                </NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Mentorship</NavigationMenuTrigger>
+                            <NavigationMenuContent className="flex flex-col p-4 w-[300px] md:w-[400px] lg:w-[500px]">
+                                <NavigationMenuLink
+                                    className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                    href="/mentorship/find-mentorship">
+                                    Find a Mentor or Mentee
+                                </NavigationMenuLink>
+                                <NavigationMenuLink
+                                    className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                    href="/mentorship/logs"
+                                >
+                                    Interaction Log
+                                </NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
             </div>
+
             <div className="flex mx-6 gap-x-6 items-center">
                 <Button className="bg-foreground text-background hover:text-foreground">
                     <Link href="/login">
@@ -76,6 +93,6 @@ export default function NavBar() {
                 <InboxIcon />
                 <UserCircle />
             </div>
-        </div>
+        </div >
     );
 }
