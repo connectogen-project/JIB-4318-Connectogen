8ee72884164a342ae8b766a0a8c79f2b128a5018
Guo, Patrick <patrickguo123@gmail.com>
2025-02-16 00:03:18 -0500

    CONNECT-147 Created notification schema

---
+++ apps/backend/models/notifications.models.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const notifSchema = new mongoose.Schema({
+    notifID: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    notifType: {
+        type: String,
+        enum: ['Incoming Connection Request', 'Accepted Connection Request',
+            'Message', 'User Profile Update'
+        ],
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    userEmail: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Date,
+        required: true
+    },
+    read: {
+        type: Boolean,
+        default: false,
+        required: true
+    }
+});
+
+const Notification = mongoose.model("Notification", notifSchema);
+export default Notification;
